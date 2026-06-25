35e6e69ae7ce11b4bddd1fedeb45592e1012977f
Sultan <astromidand2929@gmail.com>
2026-06-25 16:11:42 +0500

    feat(user): add dto for validation

---
+++ src/dto/dtos.ts
@@ -0,0 +1,30 @@
+import {IsEmail, IsString, MinLength, IsOptional} from 'class-validator'
+
+export class RegisterDto {
+    @IsEmail()
+    email!: string
+
+    @IsString()
+    @MinLength(6)
+    
+    passport!:string
+}
+
+export class LoginDto { 
+    @IsEmail({}, {message: "Некорректный формат email"})
+    email!: string
+
+    @MinLength(6, {message: "Пароль должен содержать минимум 6 символов"})
+    password!: string
+}
+
+export class UpdateProfileDto {
+    @IsOptional()
+    @IsEmail()
+    email?: string
+
+    @IsOptional()
+    @MinLength(6)
+    password?: string
+
+}
